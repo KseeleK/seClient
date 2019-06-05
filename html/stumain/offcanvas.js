4fc9e7d96344109a7e0b8c5a706674b147bd785c
KseeleK <939336352@qq.com>
2019-06-05 15:34:07 +0800

    json

--- html/stumain/offcanvas.js
+++ html/stumain/offcanvas.js
@@ -1,24 +1,26 @@
 $(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
-  });
-  // alert("1234");
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+    });
+    // alert("1234");
 
 
     // notice_json = $.ajax({url:"/jquery/test1.txt",async:false});
-  var course_json;
-  $.ajax({url:"./text.txt",async: false,success:function (result) {
-          course_json=result;
-      }});
-  // var assignment_json = $.ajax({url:"",async: false});
-  // var organization_json = $.ajax({url:"",async: false});
-  // var notice_obj=JSON.parse(notice_json);
-   //alert(course_json);
-  var course_obj = JSON.parse(course_json);
+    var course_json;
+    $.ajax({
+        url: "./client/json/", async: false, success: function (result) {
+            course_json = result;
+        }
+    });
+    // var assignment_json = $.ajax({url:"",async: false});
+    // var organization_json = $.ajax({url:"",async: false});
+    // var notice_obj=JSON.parse(notice_json);
+    //alert(course_json);
+    var course_obj = JSON.parse(course_json);
 
-  // var assignment_obj = JSON.parse(assignment_json);
-  // var organization_obj = JSON.parse(organization_json);
-  for(i=0;i<course_obj.courses.length;i++)
-    $("#courseList").append('<li class=\"list-group-item\">'+course_obj.courses[i]+"</li>")
+    // var assignment_obj = JSON.parse(assignment_json);
+    // var organization_obj = JSON.parse(organization_json);
+    for (i = 0; i < course_obj.courses.length; i++)
+        $("#courseList").append('<li class=\"list-group-item\">' + course_obj.courses[i] + "</li>")
 
 });
