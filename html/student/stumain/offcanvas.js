c0b63399b69978671a23fb2a22c2485eb2ce2aea
Jin Lianyuan <jinlianyuan@JindeMacBook-Pro.local>
2019-06-08 21:16:09 +0800

    modify href

--- html/student/stumain/offcanvas.js
+++ html/student/stumain/offcanvas.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     // notice_json = $.ajax({url:"/jquery/test1.txt",async:false});
     var course_json;
     $.ajax({
-        url: "/client/json/course/", async: false, success: function (result) {
+        url: "text.txt", async: false, success: function (result) {
             course_json = result;
         }
     });
@@ -17,11 +17,17 @@ $(document).ready(function () {
     // var notice_obj=JSON.parse(notice_json);
 
     console.log(course_json);
+    alert(course_json)
     var course_obj = JSON.parse(course_json);
 
     // var assignment_obj = JSON.parse(assignment_json);
     // var organization_obj = JSON.parse(organization_json);
-    for (i = 0; i < course_obj.courses.length; i++)
-        $("#courseList").append('<li class=\"list-group-item\" href="/client/class/'+course_obj.courses[i]+'">' + course_obj.courses[i] + "</li>")
+    for (i = 0; i < course_obj.courses.length; i++) {
+        $("#courseList").append('<li class=\"list-group-item\">' +
+            '<a href=\"/client/class/' + course_obj.courses[i] + '/\">' +
+            course_obj.courses[i] +
+            '</a>' +
+            "</li>")
+    }
 
 });
