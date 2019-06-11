a0855768fd9898e82cd58f22899919068ccf39d0
KseeleK <939336352@qq.com>
2019-06-11 21:16:25 +0800

    s

--- html/student/stumain/offcanvas.js
+++ html/student/stumain/offcanvas.js
@@ -34,9 +34,9 @@ $(document).ready(function () {
             // '<a href="/client/html/student/stucourse/index.html?class=' + course_obj.courses[i] + '">' +
             // '<a href=\"/client/stu' + course_obj.courses[i] + '\">' +
             '<a href="/client/html/student/stucourse/index.html?class=' + course_obj.courses[i] + '">' +
-        course_obj.courses[i] +
-        '</a>' +
-        "</li>"
-    )
+            course_obj.courses[i] +
+            '</a>' +
+            "</li>"
+        )
     }
-}
+});
