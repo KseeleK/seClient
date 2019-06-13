c504be81dd2852e6c127ad506fb000604bc60dc7
Jin Lianyuan <jinlianyuan@JindeMacBook-Pro.local>
2019-06-13 22:24:21 +0800

    123

--- html/student/stuhomework/offcanvas.js
+++ html/student/stuhomework/offcanvas.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
   var question_obj = JSON.parse(question_json);
     // alert(question_obj.judge.number);
 
-    for(var i=0;i<question_obj.judge.number;i++) {
+    for(var i=0;i<question_obj.homework.judge.number;i++) {
       var tmp_html;
       tmp_html = `
             <div class="">
@@ -28,14 +28,13 @@ $(document).ready(function () {
             <div>
             <div>
             <p>` +
-          question_obj.judge.problems[i].content;
-      if (question_obj.judge.problems[i].answer=="T") {
+          question_obj.homework.judge.problems[i].content;
+      if (question_obj.homework.judge.problems[i].answer=="T") {
             tmp_html = tmp_html +
             `
               <span>(2分)</span>
               </p>
               </div>
-              <input type="checkbox">
               <div class="">
                   <label>
                   <input disabled checked name="" type="radio">
@@ -52,7 +51,6 @@ $(document).ready(function () {
       }else{
           tmp_html = tmp_html +
               `<span>(2分)</span>
-              <input type="checkbox">
 
               </p>
               </div>
@@ -71,7 +69,48 @@ $(document).ready(function () {
             </div>`
       }
       $("#ques_entry").append(tmp_html);
+        for (var i = 0; i < question_obj.homework.choice.number; i++) {
+            var tmp_html1;
+            tmp_html1 = `<div class="problemListItem">
+            <span class="problemLabel">
+            2-` + (i + 1) +
+                `</span>
+            <div class="problem">
+            <div class="ques-view">
+            <p>` + question_obj.homework.choice.problems[i].content+`</p>
+                  
 
+        <ol class="ques-answer ques-choice ques-list">
+            <li>
+            <label>
+            <input disabled name="ques15" type="radio">
+            </label>` +
+                question_obj.homework.choice.problems[i].choiceA + `
+        </li>
+        <li>
+        <label>
+        <input disabled name="ques15" type="radio">
+            </label>` +
+                question_obj.homework.choice.problems[i].choiceB + `
+        </li>
+        <li>
+        <label>
+        <input disabled name="ques15" type="radio">
+            </label>` +
+                question_obj.homework.choice.problems[i].choiceC + `
+        </li>
+        <li>
+        <label>
+        <input disabled name="ques15" type="radio">
+            </label>` +
+                question_obj.homework.choice.problems[i].choiceD + `
+        </li>
+        </ol>
+        </div>
+        </div>
+        </div>`;
+            $("#ques_entry1").append(tmp_html1);
+        }
   }
   }
 );
