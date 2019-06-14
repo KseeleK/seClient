bb3279b036b44c57681430f64db8ab392524d850
Jin Lianyuan <jinlianyuan@JindeMacBook-Pro.local>
2019-06-14 19:04:23 +0800

    add action

--- html/teacher/teaaddstudent/offcanvas.js
+++ html/teacher/teaaddstudent/offcanvas.js
@@ -3,3 +3,8 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
+
+function add_stu() {
+$("#stulist").append("<input class=\"form-control\" id=\"stuname\" type=\"text\">\n" +
+    "<input class=\"form-control\" id=\"stuid\" type=\"text\">")
+}
