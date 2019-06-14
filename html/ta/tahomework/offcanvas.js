b02d2bde7fb9b7717ea9488e449d3aaa2e8b6977
Jin Lianyuan <jinlianyuan@JindeMacBook-Pro.local>
2019-06-14 18:42:28 +0800

    add stu url

--- html/ta/tahomework/offcanvas.js
+++ html/ta/tahomework/offcanvas.js
@@ -1,13 +1,16 @@
+var question_json;
 var question_obj;
+var id_map = [];
+
+
 $(document).ready(function () {
         $('[data-toggle="offcanvas"]').click(function () {
             $('.row-offcanvas').toggleClass('active')
         });
 
 
-        var question_json;
         $.ajax({
-            url: "/client/json/questions/", async: false,
+            url: "text.txt", async: false,
             xhrFields: {
                 withCredentials: true // 这里设置了withCredentials
             },
@@ -17,8 +20,9 @@ $(document).ready(function () {
         });
         question_obj = JSON.parse(question_json);
         // alert(question_obj.judge.number);
-
-        for (var i = 0; i < question_obj.judge.number; i++) {
+        // alert(question_obj.homework.judge.number);
+        for(var i=0;i<question_obj.homework.judge.number;i++) {
+            id_map[i] = question_obj.homework.judge.problems[i].id;
             var tmp_html;
             tmp_html = `
             <div class="">
@@ -29,8 +33,8 @@ $(document).ready(function () {
             <div>
             <div>
             <p>` +
-                question_obj.judge.problems[i].content;
-            if (question_obj.judge.problems[i].answer == "T") {
+                question_obj.homework.judge.problems[i].content;
+            if (question_obj.homework.judge.problems[i].answer == "T") {
                 tmp_html = tmp_html +
                     `
               <span>(2分)</span>
@@ -38,12 +42,12 @@ $(document).ready(function () {
               </div>
               <div class="">
                   <label>
-                  <input disabled checked name="" type="radio">
+                  <input  checked name="` + i + `" type="radio" value="T">
                   T
                   </label>
                   &nbsp; &nbsp; &nbsp; &nbsp;
           <label>
-              <input disabled name="" type="radio">
+              <input  name="` + i + `" type="radio" value="F">
                   F
                   </label>
                   </div>
@@ -52,18 +56,17 @@ $(document).ready(function () {
             } else {
                 tmp_html = tmp_html +
                     `<span>(2分)</span>
-              <input type="checkbox" id="` + (i) + `">
 
               </p>
               </div>
               <div class="">
                   <label>
-                  <input disabled name="" type="radio">
+                  <input name="` + i + `" type="radio" value="T">
                   T
                   </label>
                   &nbsp; &nbsp; &nbsp; &nbsp;
           <label>
-              <input disabled checked name="" type="radio">
+              <input checked name="` + i + `" type="radio" value="F">
                   F
                   </label>
                   </div>
@@ -72,8 +75,9 @@ $(document).ready(function () {
             }
             $("#ques_entry").append(tmp_html);
         }
-        // alert(question_obj.choice.number);
-        for (var i = 0; i < question_obj.choice.number; i++) {
+        for (var i = 0; i < question_obj.homework.choice.number; i++) {
+            id_map[i+question_obj.homework.judge.number] = question_obj.homework.choice.problems[i].id;
+
             var tmp_html1;
             tmp_html1 = `<div class="problemListItem">
             <span class="problemLabel">
@@ -81,33 +85,33 @@ $(document).ready(function () {
                 `</span>
             <div class="problem">
             <div class="ques-view">
-            <p>` + question_obj.choice.problems[i].content + `"></p>
+            <p>` + question_obj.homework.choice.problems[i].content+`</p>
                   
 
         <ol class="ques-answer ques-choice ques-list">
             <li>
             <label>
-            <input disabled name="ques15" type="radio">
+            <input  name="`+(i+question_obj.homework.judge.number)+`" type="radio" value="A">
             </label>` +
-                question_obj.choice.problems[i].choiceA + `
+                question_obj.homework.choice.problems[i].choiceA + `
         </li>
         <li>
         <label>
-        <input disabled name="ques15" type="radio">
+        <input  name="`+(i+question_obj.homework.judge.number)+`" type="radio" value="B">
             </label>` +
-                question_obj.choice.problems[i].choiceB + `
+                question_obj.homework.choice.problems[i].choiceB + `
         </li>
         <li>
         <label>
-        <input disabled name="ques15" type="radio">
+        <input  name="`+(i+question_obj.homework.judge.number)+`" type="radio" value="C">
             </label>` +
-                question_obj.choice.problems[i].choiceC + `
+                question_obj.homework.choice.problems[i].choiceC + `
         </li>
         <li>
         <label>
-        <input disabled name="ques15" type="radio">
+        <input  name="`+(i+question_obj.homework.judge.number)+`" type="radio" value="D">
             </label>` +
-                question_obj.choice.problems[i].choiceD + `
+                question_obj.homework.choice.problems[i].choiceD + `
         </li>
         </ol>
         </div>
@@ -115,6 +119,7 @@ $(document).ready(function () {
         </div>`;
             $("#ques_entry1").append(tmp_html1);
         }
+
     }
 );
 
@@ -126,19 +131,18 @@ $("#upload").click(function () {
     // }
     var count = 0;
     var count1 = 0;
-    var arr = [];
-    $.each($('input:checkbox'), function () {
-        if (this.checked) {
-            // alert(count);
-            // window.alert("你选了："+ $('input[type=checkbox]:checked').length+"个，其中有："+$(this).val());
-            arr[count1] = count;
-            count1++;
-        }
-
-        count++;
-    });
-    var url = "/client/json/homework/generate/";
-    $.post(url, {QuestionIDs: arr}, function (resultJSONObject) {
+    var arr = new Array();
+    var arr1 = new Array();
+    for(var i = 0; i < question_obj.homework.choice.number+question_obj.homework.judge.number;i++) {
+        var tmp = i.toString();
+        arr[i] = ($("input[name="+tmp+"]:checked").val());
+        arr1[i] = id_map[i];
+    }
+    // alert(arr);
+    // alert(arr1);
+    var url = "/client/json/homework/submit/";
+    // alert($("#homeName").val());
+    $.post(url, {HomeworkID: arr1,HomeworkAns:arr,Name:$("#homeName").val()}, function (resultJSONObject) {
         if (resultJSONObject.success) {
             $.messager.alert("系统提示", "添加成功", "info");
         } else {
